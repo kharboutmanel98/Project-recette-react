c9e8da4c61687f547a753faa73230ea10c6f3065
kharboutmanel98 <kharbout2017@gmail.com>
2023-11-10 22:36:42 +0100

    add recette in wishlist

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,18 +1,29 @@
+import styles from "./Recipe.module.scss";
+import { useState } from "react";
 
-import styles from "./Recipe.module.scss"
-function Recipe({title, image})  {
+
+function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+  function handleClick() {
+    setLiked(!liked);
+  }
   return (
-    <div className={styles.recipe}>
-    <div className={styles.imageContainer}>
+    <div onClick={handleClick} className={styles.recipe}>
+      <div className={styles.imageContainer}>
         <img src={image} alt="recipe" />
-        <div className={`${styles.recipeTitle} d-flex flex-row align-items-center justify-content-center`}>
-            <h3>
-              {title}
-            </h3>
-        </div>
+      </div>
+      <div
+        className={`${styles.recipeTitle} d-flex flex-column align-items-center justify-content-center`}
+      >
+        <h3 className="mb-10">{title}</h3>
+        <i
+          className={`fa-solid fa-heart ${
+            liked ? "text-primary" : " "
+          }`}
+        ></i>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Recipe
+export default Recipe;
